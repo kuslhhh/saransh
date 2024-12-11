eed48ac98936a007d68bb834b024df6970545e3b
kuslhhh <kushaljadhav7745@gmail.com>
2024-12-11 23:04:09 +0530

    jsx

--- components/search.jsx
+++ components/search.jsx
@@ -1,18 +1,15 @@
 "use client";
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { useLazyGetSummaryQuery } from "@/utils/services/article";
-import Loading from "../app/loading";
+import Loading from "../../app/loading";
 import { motion } from "framer-motion";
 
-// Define types for article and articles state
-interface Article {
-  url: string;
-  summary: string;
-}
-
 const Search = () => {
-  const [article, setArticle] = useState<Article>({ url: "", summary: "" });
-  const [allArticles, setAllArticles] = useState<Article[]>([]);
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+  const [allArticles, setAllArticles] = useState([]);
 
   // RTK lazy query
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
@@ -21,43 +18,37 @@ const Search = () => {
 
   // Load data from localStorage on mount
   useEffect(() => {
-    const articlesFromLocalStorage = localStorage.getItem("articles");
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
     if (articlesFromLocalStorage) {
-      setAllArticles(JSON.parse(articlesFromLocalStorage));
+      setAllArticles(articlesFromLocalStorage);
     }
   }, []);
 
-  // Submit handler
-  const handleSubmit = useCallback(async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const existingArticle = allArticles.find(
       (item) => item.url === article.url
     );
 
-    if (existingArticle) {
-      return setArticle(existingArticle);
+    if (existingArticle) return setArticle(existingArticle);
+
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      // update state and local storage
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
-
-    try {
-      const { data } = await getSummary({ articleUrl: article.url });
-      if (data?.summary) {
-        const newArticle = { ...article, summary: data.summary };
-        const updatedAllArticles = [newArticle, ...allArticles];
-
-        // Update state and local storage
-        setArticle(newArticle);
-        setAllArticles(updatedAllArticles);
-        localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
-      }
-    } catch (err) {
-      console.error("Error fetching summary:", err);
-    }
-  }, [allArticles, article.url, getSummary]);
-
+  };
   return (
     <section className="wrapper grid grid-cols-1 lg:grid-cols-2 gap-8 my-auto min-h-[52vh]">
-      {/* Input Form */}
       <div className="flex flex-col gap-8">
         <form
           onSubmit={handleSubmit}
@@ -73,7 +64,7 @@ const Search = () => {
           />
           <button
             type="submit"
-            className="flex gap-2 text-2xl px-1 flex-center underline text-background hover:text-foreground transition-all duration-300 my-auto"
+            className=" flex gap-2 text-2xl px-1 flex-center underline text-background hover:text-foreground transition-all duration-300 my-auto"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -89,10 +80,22 @@ const Search = () => {
                 d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
               />
             </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 md:hidden"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
+              />
+            </svg>
           </button>
         </form>
-
-        {/* Display Saved Articles */}
         <div className="flex flex-col gap-4">
           {allArticles.map((item, index) => (
             <motion.div
@@ -101,17 +104,14 @@ const Search = () => {
               initial={{ y: 20, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{ duration: 1, ease: [0.22, 0.5, 0.36, 1] }}
-              className="cursor-pointer"
             >
-              <p className="line-clamp-1 text-sm md:text-base text-primary/50 hover:text-foreground hover:underline underline-offset-4 transition-all duration-300">
+              <p className="line-clamp-1 text-sm md:text-base cursor-pointer text-primary/50 hover:text-foreground hover:underline underline-offset-4 transition-all duration-300">
                 {item.url}
               </p>
             </motion.div>
           ))}
         </div>
       </div>
-
-      {/* Display Article Summary */}
       <div className="w-full flex flex-col gap-5" id="summary">
         {isFetching ? (
           <Loading />
@@ -125,8 +125,8 @@ const Search = () => {
             <motion.div
               className={`flex flex-col ${
                 readMore
-                  ? "absolute top-0 left-0 size-full z-20 bg-background/80 text-background transition-all duration-300"
-                  : ""
+                  ? "top-0 bg-background/80 left-0 size-full z-20 transition-all duration-300 text-background absolute my-auto"
+                  : null
               }`}
               initial={{ y: 40, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
@@ -136,48 +136,56 @@ const Search = () => {
               <div
                 className={`${
                   readMore
-                    ? "mx-auto px-[6vw] py-[4vh] bg-primary/85 border-2 border-background shadow-[-12px_12px_0px_rgba(244,62,11,1)]"
-                    : ""
+                    ? " mx-auto px-[6vw] py-[4vh] my-auto bg-primary/85 border-2 border-background shadow-[-12px_12px_0px_rgba(244,62,11,1)]"
+                    : null
                 }`}
               >
-                <div className={`flex flex-col gap-6`}>
-                  <div className="flex justify-between">
-                    <h2 className="text-4xl md:text-5xl text-foreground underline">
+                <div
+                  className={`${
+                    readMore ? "wrapper" : null
+                  } flex flex-col gap-6 justify-start`}
+                >
+                  <div className="justify-between flex ">
+                    <h2 className="text-4xl md:text-5xl text-foreground underline hover:underline-offset-8">
                       Summary:
                     </h2>
-                    {readMore && (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        onClick={() => setReadMore(false)}
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-10 h-10 cursor-pointer hover:rotate-180 hover:text-foreground transition-all duration-300"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M6 18 18 6M6 6l12 12"
-                        />
-                      </svg>
-                    )}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      onClick={() => setReadMore(!readMore)}
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className={`w-10 h-10 my-auto cursor-pointer ${
+                        readMore ? null : "hidden"
+                      } hover:rotate-180 hover:text-foreground transition-all duration-300`}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18 18 6M6 6l12 12"
+                      />
+                    </svg>
                   </div>
                   <p
                     className={`text-base md:text-lg ${
-                      readMore ? "text-background" : "text-primary line-clamp-6"
+                      readMore
+                        ? " text-background"
+                        : "text-primary line-clamp-6"
                     }`}
                   >
                     {article.summary}
                   </p>
-                  {!readMore && (
+                  <div>
                     <button
-                      className="bg-primary/80 hover:bg-primary/90 px-4 py-2 text-foreground font-bold border-2 shadow-[-6px_6px_0px_rgba(244,62,11,0.8)] hover:shadow-[-6px_-6px_0px_rgba(244,62,11,0.8)] transition-all duration-300"
-                      onClick={() => setReadMore(true)}
+                      className={`${
+                        readMore ? "hidden" : null
+                      }  bg-primary/80 hover:bg-primary/90 px-4 py-2 text-foreground font-bold border-2 shadow-[-6px_6px_0px_rgba(244,62,11,0.8)] hover:shadow-[-6px_-6px_0px_rgba(244,62,11,0.8)] transition-all duration-300 border-background`}
+                      onClick={() => setReadMore(!readMore)}
                     >
-                      Read More
+                      {readMore ? "Read Less" : "Read More"}
                     </button>
-                  )}
+                  </div>
                 </div>
               </div>
             </motion.div>
